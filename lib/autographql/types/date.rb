8e653bc6ca3dfc26e3155ba7edaf1a75ae366a64
dpepper <pepper.daniel@gmail.com>
2018-11-13 15:21:17 -0800

    .

--- lib/autographql/types/date.rb
+++ lib/autographql/types/date.rb
@@ -1,6 +1,6 @@
 GraphQL::Types::DATE = GraphQL::ScalarType.define do
   name 'DATE'
-  description 'Date formatted as YYYY-MM-DD'
+  description 'Date formatted as YYYY-MM-DD  (ISO 8601)'
 
   coerce_result ->(value, ctx) do
     value.to_s
