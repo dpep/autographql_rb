ebe55d051494aaa359b02583ff9313ce6d3feea3
dpepper <pepper.daniel@gmail.com>
2018-11-15 17:41:40 -0800

    cleanup

--- test/support/db/schema.rb
+++ test/support/db/schema.rb
@@ -9,12 +9,12 @@ ActiveRecord::Schema.define do
     t.string :name
     t.json :data
     t.decimal :age
+    t.integer :location_id
     t.datetime :created_at, default: -> { 'CURRENT_TIMESTAMP' }
   end
 
   create_table :pets do |t|
     t.string :name
-
     t.integer :person_id
     t.integer :location_id
   end
@@ -22,9 +22,7 @@ ActiveRecord::Schema.define do
   create_table :locations do |t|
     t.string :name
     t.date :founded
-
-    t.integer :object_id
-    t.string :object_type
+    t.integer :location_id
   end
 end
 
@@ -32,7 +30,7 @@ end
 # define models
 class Person < ActiveRecord::Base
   has_many :pets
-  has_one :location, :as => :object
+  belongs_to :location
 
   def awesomeness
     100
@@ -50,14 +48,11 @@ end
 
 class Pet < ActiveRecord::Base
   belongs_to :person
-  has_one :location, :as => :object
+  belongs_to :location
 end
 
 class Location < ActiveRecord::Base
-  belongs_to :location, {
-    :foreign_key => :object_id,
-    :foreign_type => :object_type,
-  }
+  belongs_to :location
 end
 
 
